7af136ff4a81cb864505fdc426635c9e11f5a5e1
Haojian_Qin <haojian_qin@shtdo.com>
2024-06-05 11:22:17 +0800

    feat(app):add the test code

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,3 +1,10 @@
+import TitleBar from "@src/components/TitleBar/TitleBar";
+
 export default function app(){
-    return (<h1>Hello Without &apos;Giraffe IDE&apos; World</h1>);
+    return (
+        <>
+            <h1>Hello Without &apos;Giraffe IDE&apos; World</h1>
+            <TitleBar />
+        </>
+    );
 }
